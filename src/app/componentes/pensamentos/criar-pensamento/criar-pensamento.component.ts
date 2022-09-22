5b5a06acb28d3186ff7c49dc950f51f94db96ed4
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2022-09-22 05:53:00 -0300

    Adding interpolation, property bind and salvarPensamento()

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  salvarPensamento() {
+    alert("Pensamento salvo")
+  }
 }
